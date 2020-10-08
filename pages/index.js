3e3444fc7778904a72c277135eb5612f9e40c413
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-10-08 23:03:27 +0700

    setting up project done

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto mt-4">
+      <Head>
+        <title>BWA MICRO</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main >
+        <div>
+          halaman utama
+        </div>
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
